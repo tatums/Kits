63f8dfb7693a90ec92f8b8f390624dd5c4f60683
Tatum Szymczak <t.szymczak@tukaiz.com>
2012-06-07 07:15:39 -0500

    More descriptive validation errors

--- app/models/kit_relationship.rb
+++ app/models/kit_relationship.rb
@@ -7,7 +7,7 @@ class KitRelationship < ActiveRecord::Base
 
   def parent_and_child_cannot_be_same
     if parent_id == child_id
-      errors.add(:id, "Computer Says Noooooo")
+      errors.add(:id, "Computer Says Noooooo... Parent & Child cannot be the same.")
     end
   end
 
@@ -22,13 +22,13 @@ class KitRelationship < ActiveRecord::Base
 
   def child_can_not_have_a_parent_as_a_child
     if parent.parents_loop(parent.parents).include?(child.id)
-      errors.add(:id, "Computer Says Noooooo")
+      errors.add(:id, "Computer Says Noooooo... Parent as a child.")
     end
   end
 
   def parent_can_not_have_a_child_as_a_parent
     if child.children_loop(child.children).include?(parent.id)
-      errors.add(:id, "Computer Says Noooooo")
+      errors.add(:id, "Computer Says Noooooo... Child as a parent. ")
     end
   end
 
